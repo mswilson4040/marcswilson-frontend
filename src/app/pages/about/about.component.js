261f1c5fd7477b385cba4eeac834c5b7fbf0c4af
Marc Wilson <mwilson@marcswilson.com>
2017-07-17 20:25:49 -0700

    initial commit of about page addition (social media icons)

--- src/app/pages/about/about.component.js
+++ src/app/pages/about/about.component.js
@@ -10,9 +10,13 @@ var AboutComponent = (function () {
     function AboutComponent() {
     }
     AboutComponent.prototype.ngOnInit = function () {
+        // TODO: Clean this up. This is nonsense!
+        // TODO: Replace jQuery animation with @keyframes animation
         $('#imageContainer').find('img').fadeIn('slow');
         var containers = $('.content-container');
+        var icons = $('.icon-container');
         var delay = 500;
+        var delay2 = 500;
         var _loop_1 = function(i) {
             $(containers[i]).delay(delay).toggle('slide', { direction: 'left' }, 700, function () {
                 $(containers[i]).closest('.content-row').find('img').delay(100).fadeIn(500);
@@ -22,6 +26,13 @@ var AboutComponent = (function () {
         for (var i = 0; i < containers.length; i++) {
             _loop_1(i);
         }
+        for (var i = 0; i < icons.length; i++) {
+            $(icons[i]).delay(delay2).show('scale', { percent: 50 }, 1000);
+            delay2 += 100;
+        }
+    };
+    AboutComponent.prototype.navigate = function (url) {
+        window.open(url, '_blank');
     };
     AboutComponent = __decorate([
         core_1.Component({
