7002451b941502ea8055ad47cc1830456003b8e3
Marc Wilson <mwilson@marcswilson.com>
2017-09-06 19:52:23 -0700

    added ability to filter entries by date

--- src/app/pages/portfolio/time-tracker/time-tracker-timesheet/time-tracker-timesheet.component.ts
+++ src/app/pages/portfolio/time-tracker/time-tracker-timesheet/time-tracker-timesheet.component.ts
@@ -1,22 +1,23 @@
-import {AfterViewChecked, AfterViewInit, Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TimeTrackerService } from '../services/time-tracker.service';
-import {Company, Entry, Project} from '../classes/company';
-import { MdDialog} from '@angular/material';
+import { Company, Entry, Project } from '../classes/company';
+import { MdDialog } from '@angular/material';
 import { EntryDialogComponent } from '../dialogs/entry-dialog/entry-dialog.component';
 import { ErrorDialogComponent } from '../../../../shared-components/error-dialog/error-dialog.component';
-import {ConfirmDialogComponent} from '../../../../shared-components/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialogComponent } from '../../../../shared-components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-time-tracker-timesheet',
   templateUrl: './time-tracker-timesheet.component.html',
   styleUrls: ['./time-tracker-timesheet.component.scss']
 })
-export class TimeTrackerTimesheetComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class TimeTrackerTimesheetComponent implements OnInit {
 
   public companies: Array<Company> = new Array<Company>();
   public currentDate: Date = new Date();
   public selectedTabIndex = 0;
   public selectedCompany: Company = new Company();
+  public filteredEntries: Array<Entry> = new Array<Entry>();
   constructor(private _timeTrackerService: TimeTrackerService, private _dialog: MdDialog) {
   }
 
@@ -26,10 +27,6 @@ export class TimeTrackerTimesheetComponent implements OnInit, AfterViewInit, Aft
     }, error => {
       alert(error.message);
     });
-  }
-  ngAfterViewInit(): void {
-  }
-  ngAfterViewChecked(): void {
   }
   addNewEntry(): void {
     const dialogRef = this._dialog.open(EntryDialogComponent, { height: '60%', width: '30%'});
@@ -50,6 +47,13 @@ export class TimeTrackerTimesheetComponent implements OnInit, AfterViewInit, Aft
   onCompanyChange(): void {
     this._timeTrackerService.getEntriesByCompany(this.selectedCompany).then( entries => {
       this.selectedCompany.entries = entries;
+      if (this.selectedTabIndex !== 0) {
+        this.filteredEntries = this.selectedCompany.entries.filter(e => {
+          return e.date.getDay() === this.currentDate.getDay();
+        });
+      } else {
+        this.filteredEntries = this.selectedCompany.entries;
+      }
     }, error => {
       const edr = this._dialog.open(ErrorDialogComponent);
       edr.componentInstance.error = error;
@@ -86,5 +90,11 @@ export class TimeTrackerTimesheetComponent implements OnInit, AfterViewInit, Aft
       }
     });
 
+  }
+  onTabChanged(index: number): void {
+    this.selectedTabIndex = index;
+    this.filteredEntries = this.selectedCompany.entries.filter( e => {
+      return e.date.getDay() === this.selectedTabIndex;
+    });
   }
 }
