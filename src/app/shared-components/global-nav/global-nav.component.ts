06b6a6706432b7c4c21630386d5f77bae5fd4c88
Marc Wilson <mwilson@marcswilson.com>
2017-10-20 21:27:46 -0700

    added styles to global nav

--- src/app/shared-components/global-nav/global-nav.component.ts
+++ src/app/shared-components/global-nav/global-nav.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ContactFormDialogComponent } from '../contact-form-dialog/contact-form-dialog.component';
 import { MdDialog } from '@angular/material';
 import { EmailService } from '../../shared-services/email.service';
 import { AuthService } from '../../shared-services/auth.service';
 import { AuthenticationResponse } from '../../shared-classes/authentication-response';
+import { UIService } from '../../shared-services/ui.service';
 
 @Component({
   selector: 'app-global-nav',
@@ -15,14 +16,23 @@ export class GlobalNavComponent implements OnInit {
   public path: string = null;
   public authResponse: AuthenticationResponse = new AuthenticationResponse();
   public router: Router;
+  public outOfView = false;
   constructor(private _router: Router, private _dialog: MdDialog, private _emailService: EmailService,
-              private _authService: AuthService) {
+              private _authService: AuthService, private _uiService: UIService, private _elementRef: ElementRef) {
     this.router = this._router;
   }
 
   ngOnInit() {
     this._authService.authWatch$.subscribe( auth => {
       this.authResponse = auth;
+    });
+    this._uiService.scrollService.subscribe( evt => {
+      const isElementInView = this._uiService.isElementInView(this._elementRef);
+      if (!isElementInView) {
+        this.outOfView = true;
+      } else {
+        this.outOfView = false;
+      }
     });
   }
   openContactForm(): void {
