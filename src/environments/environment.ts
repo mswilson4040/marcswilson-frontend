080142c656ff4c7a0e936d797fc301623fd0f9aa
Marc Wilson <mwilson@marcswilson.com>
2017-11-04 19:02:46 -0700

    removed auth0 config; moved keys inside environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,13 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-import {AUTH_CONFIG} from './auth0.config';
-
 export const environment = {
   production: false,
   API_PATH: 'http://localhost:3000/api',
   ROOT_URL: 'http://localhost:4200',
-  AUTH0_CLIENTID: AUTH_CONFIG.clientID,
-  AUTH0_DOMAIN: AUTH_CONFIG.domain,
-  AUTH0_CALLBACK: AUTH_CONFIG.callbackURL,
+  AUTH0_CLIENTID: '3iT4JDvOLb73IFNsh0EPH2t9WTNhy0ep',
+  AUTH0_DOMAIN: 'marcswilson.auth0.com',
+  AUTH0_CALLBACK: 'http://localhost:4200/callback',
   AUTH0_CALLBACK_ROUTE_KEY: 'marcswilson_auth0_callback_route',
   AUTH0_AUTH_RESPONSE_MODEL_KEY: 'marcswilson_auth0_user_model'
 };
