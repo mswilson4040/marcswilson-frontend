f8048beef1da78044b087e15602adc6d1192ed31
Marc Wilson <mwilson@marcswilson.com>
2017-11-21 19:56:30 -0800

    navbar fixes/styling; added inputs to apiexplorer

--- src/app/pages/portfolio/apiexplorer/apiexplorer.component.ts
+++ src/app/pages/portfolio/apiexplorer/apiexplorer.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apiexplorer.component.scss']
 })
 export class ApiExplorerComponent implements OnInit {
-
+  public activePanelId = 0;
   constructor() { }
 
   ngOnInit() {
   }
-
+  onPanelOpen(id: number): void {
+    if (id) {
+      this.activePanelId = id;
+    } else {
+      this.activePanelId = 0;
+    }
+  }
+  onPanelClose(id: number): void {
+    if (this.activePanelId === id) {
+      this.activePanelId = 0;
+    }
+  }
 }
