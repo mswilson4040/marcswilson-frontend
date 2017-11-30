2fdba7e59309ce70050adcbdc4577a4b8a2ab8bf
Marc Wilson <mwilson@marcswilson.com>
2017-11-29 20:22:57 -0800

    started working on the UI for json viewer

--- src/app/shared-components/json-viewer/json-viewer.component.ts
+++ src/app/shared-components/json-viewer/json-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as UUID from 'uuid/v1';
 
 @Component({
   selector: 'app-json-viewer',
@@ -22,8 +23,9 @@ export class JsonViewerComponent implements OnInit {
     const indentSize = 4;
     let lines = [];
     if (Array.isArray(obj)) {
+      const id = UUID();
       const arrayPrefix = arrayName ? arrayName : 'Array';
-      lines.push( new Line(`${arrayPrefix}: [`, indentCount) );
+      lines.push( new Line(`${arrayPrefix}: [`, indentCount, true, `open_${id}`) );
       indentCount += indentSize;
       for (let i = 0; i < obj.length; i++) {
         const item = obj[ i ];
@@ -33,17 +35,18 @@ export class JsonViewerComponent implements OnInit {
           lines = lines.concat(this.getLines(item, null, indentCount));
         } else {
           const suffix = (i + 1) === obj.length ? '' : ',';
-          lines.push(new Line(`${item}${suffix}`, indentCount));
+          lines.push(new Line(`${item}${suffix}`, indentCount, false, id));
         }
       }
       indentCount -= indentSize;
-      lines.push(new Line(']', indentCount));
+      lines.push(new Line('],', indentCount, false, `close_${id}`));
     } else if (typeof obj === 'object') {
-      lines.push( new Line('{', indentCount) );
+      const id = UUID();
+      lines.push( new Line('{', indentCount, true, `open_${id}`) );
       indentCount += indentSize;
       let counter = 0;
       for (const o in obj) {
-        if (obj[o]) {
+        if (obj[o] || obj[o] === 0 || typeof obj[o] === 'string') {
           counter++;
           const item = obj[o];
           if (Array.isArray(item)) {
@@ -51,16 +54,25 @@ export class JsonViewerComponent implements OnInit {
           } else if (typeof item === 'object') {
             lines = lines.concat(this.getLines(item, null, indentCount));
           } else {
-            lines.push( new Line(`${o}: ${item},`, indentCount) );
+            const suffix = counter === Object.keys(obj).length ? '' : ',';
+            lines.push( new Line(`${o}: ${item}${suffix}`, indentCount, false, id) );
           }
+        } else {
+          console.log(obj[o]);
+          console.log(o);
+          console.log(typeof obj[o]);
         }
       }
       indentCount -= indentSize;
+      lines.push( new Line('},', indentCount, false, `close_${id}`) );
     } else {
       alert(obj);
     }
 
     return lines;
+  }
+  toggleBlock(line: Line): void {
+    line.collapsed = !line.collapsed;
   }
 }
 
@@ -68,9 +80,14 @@ class Line {
   public text: string = null;
   public indentCount: number = null;
   public arrayOfNothing: any[] = [];
-  constructor (private _text: string, private _indentCount: number) {
+  public collapsable: boolean = null;
+  public blockId: string = null;
+  public collapsed = false;
+  constructor (_text: string, _indentCount: number, _collapsable: boolean, _blockId: string) {
     this.text = _text;
     this.indentCount = _indentCount;
     this.arrayOfNothing = Array(this.indentCount).fill(null);
+    this.collapsable = _collapsable;
+    this.blockId = _blockId;
   }
 }
