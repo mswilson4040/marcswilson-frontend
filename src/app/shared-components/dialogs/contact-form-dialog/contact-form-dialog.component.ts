8fced2cc1ad24e46b7d39ab31b8ef1654b62a93f
Marcus Wilson <mswilsonr4040@hotmail.com>
2018-01-07 17:50:11 -0800

    integrated ckeditor

--- src/app/shared-components/dialogs/contact-form-dialog/contact-form-dialog.component.ts
+++ src/app/shared-components/dialogs/contact-form-dialog/contact-form-dialog.component.ts
@@ -7,6 +7,7 @@ import { MailMessage } from '../../../models/mail-message';
 import { FormControl, Validators } from '@angular/forms';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { PageBreakpoints } from '../../../enums/page-breakpoints.enum';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic/build/ckeditor';
 
 @Component({
   selector: 'app-contact-form-dialog',
@@ -17,18 +18,17 @@ export class ContactFormDialogComponent implements OnInit {
   public mailMessage: MailMessage = new MailMessage();
   public fromCtrl: FormControl;
   public subjectCtrl: FormControl;
-  public messageCtrl: FormControl;
+  public editor: any = null;
   constructor(private _matDialogRef: MatDialogRef<ContactFormDialogComponent>, private _emailService: EmailService,
               private _matDialog: MatDialog, private _breakPointObserver: BreakpointObserver) {
     this.fromCtrl = new FormControl('', [ Validators.required] );
     this.subjectCtrl = new FormControl('', [ Validators.required] );
-    this.messageCtrl = new FormControl('', [ Validators.required] );
   }
 
   ngOnInit() {
     this.mailMessage.to = environment.myEmail;
     if (this._breakPointObserver.isMatched(PageBreakpoints.ExtraLarge)) {
-      this._matDialogRef.updateSize('30%', 'auto');
+      this._matDialogRef.updateSize('25%', 'auto');
     }
     if (this._breakPointObserver.isMatched(PageBreakpoints.Large)) {
       this._matDialogRef.updateSize('35%', 'auto');
@@ -43,11 +43,14 @@ export class ContactFormDialogComponent implements OnInit {
       this._matDialogRef.updateSize('90%', 'auto');
     }
 
+    ClassicEditor.create( document.querySelector( '#editor') ).then( _editor => {
+      this.editor = _editor;
+    });
+
   }
   sendEmail(): void {
-    if (!this.fromCtrl.hasError('required') && !this.subjectCtrl.hasError('required')
-      && !this.messageCtrl.hasError('required')) {
-
+    if (!this.fromCtrl.hasError('required') && !this.subjectCtrl.hasError('required')) {
+      this.mailMessage.message = this.editor.getData();
       this._emailService.sendEmail( this.mailMessage ).then( result => {
         this._matDialogRef.close();
       }, error => {
@@ -57,7 +60,6 @@ export class ContactFormDialogComponent implements OnInit {
     } else {
       this.fromCtrl.markAsTouched();
       this.subjectCtrl.markAsTouched();
-      this.messageCtrl.markAsTouched();
     }
   }
   cancel(): void {
