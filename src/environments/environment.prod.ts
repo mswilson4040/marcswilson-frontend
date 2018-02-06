fdc17fa87c7976f1c0777a8d5b96176af5198905
Marcus Wilson <mswilson4040@hotmail.com>
2018-02-05 20:00:55 -0800

    upgraded to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@
 export const environment = {
   production: true,
   API_PATH: 'http://api.marcswilson.com',
-  ROOT_URL: 'http://marcswilson.com',
+  ROOT_URL: 'https://marcswilson.com',
   AUTH0_CLIENTID: '3iT4JDvOLb73IFNsh0EPH2t9WTNhy0ep',
   AUTH0_DOMAIN: 'marcswilson.auth0.com',
-  AUTH0_CALLBACK: 'http://marcswilson.com/callback',
+  AUTH0_CALLBACK: 'https://marcswilson.com/callback',
   AUTH0_CALLBACK_ROUTE_KEY: 'marcswilson_auth0_callback_route',
   AUTH0_AUTH_RESPONSE_MODEL_KEY: 'marcswilson_auth0_user_model',
   NODE_SERVER: 'http://54.172.164.162:3000',
