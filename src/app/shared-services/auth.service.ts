9ad891965946160c371ebe78c27019cfb5cf2ce3
Marcus Wilson <mswilson4040@hotmail.com>
2018-02-06 21:21:25 -0800

    forgot some files

--- src/app/shared-services/auth.service.ts
+++ src/app/shared-services/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthService {
+  private API_PATH = `${environment.API_PATH}/auth`;
+  constructor(private _router: Router, private _httpClient: HttpClient) {
 
-  constructor(private _router: Router) {
-
+  }
+  login(username: string, hash: string): Promise<any> {
+    return new Promise( (resolve, reject) => {
+      this._httpClient.post(`${this.API_PATH}/login`, { username: username, hash: hash }).subscribe( _result => {
+        resolve(_result);
+      }, error => {
+        reject(error);
+      });
+    });
   }
 
 }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as CryptoJS from 'crypto-js';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../shared-services/auth.service';
+import { MatDialog } from '@angular/material';
+import { ErrorDialogComponent } from '../../shared-components/dialogs/error-dialog/error-dialog.component';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public username: string = null;
+  public password: string = null;
+  constructor(private _authService: AuthService, private _matDialog: MatDialog) { }
 
   ngOnInit() {
   }
-
+  login(): void {
+    if (this.username && this.password) {
+      this.password = CryptoJS.AES.encrypt(this.password, environment.clientKey).toString();
+      this._authService.login(this.username, this.password).then( _result => {
+        console.log(_result);
+      }, error => {
+        this._matDialog.open(ErrorDialogComponent, { data: error });
+      });
+    }
+  }
 }
