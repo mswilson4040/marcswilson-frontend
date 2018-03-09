794a4c61070b901a8bb7d54ee8345b25ac09b5c3
Marcus Wilson <mswilson4040@hotmail.com>
2018-03-08 20:29:53 -0800

    added https to endpoint url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  API_PATH: 'http://api.marcswilson.com',
+  API_PATH: 'https://api.marcswilson.com',
   ROOT_URL: 'https://marcswilson.com',
   AUTH0_CLIENTID: '3iT4JDvOLb73IFNsh0EPH2t9WTNhy0ep',
   AUTH0_DOMAIN: 'marcswilson.auth0.com',
@@ -9,6 +9,6 @@ export const environment = {
   AUTH0_CALLBACK_ROUTE_KEY: 'marcswilson_auth0_callback_route',
   AUTH0_AUTH_RESPONSE_MODEL_KEY: 'marcswilson_auth0_user_model',
   NODE_SERVER: 'http://54.172.164.162:3000',
-  myEmail: 'marcw@marcswilson.com',
+  myEmail: 'marc@marcswilson.com',
   clientKey: 'clientKeyProd'
 };
