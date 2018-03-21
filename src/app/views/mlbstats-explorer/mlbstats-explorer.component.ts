88b6cfa333f03c37b6583463d26fd3be662c06c6
Marcus Wilson <mswilson4040@hotmail.com>
2018-03-20 19:31:09 -0700

    d3 maps

--- src/app/views/mlbstats-explorer/mlbstats-explorer.component.ts
+++ src/app/views/mlbstats-explorer/mlbstats-explorer.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
-import { select } from 'd3';
+import { select, geoMercator, geoPath } from 'd3';
 import { ChadwickService } from '../../shared-services/chadwick.service';
 import { UIService } from '../../shared-services/ui.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mlbstats-explorer',
@@ -10,21 +11,26 @@ import { UIService } from '../../shared-services/ui.service';
   providers: [ ChadwickService ]
 })
 export class MlbstatsExplorerComponent implements OnInit {
-  public seasons: string[] = [];
-  public selectedYear = '1956';
-  constructor(private _chadwickService: ChadwickService, private _uiService: UIService) { }
+
+  constructor(private _chadwickService: ChadwickService, private _uiService: UIService, private _httpClient: HttpClient) { }
 
   async ngOnInit() {
-    const seasons = await this._chadwickService.getSeasons();
-    this.seasons = seasons.reverse();
-  }
-  onSliderChange(e) {
-    this.selectedYear = e.value;
-    const nativeElement = document.querySelector(`#season_${this.selectedYear}`);
-    if (nativeElement) {
-      const element = new ElementRef( nativeElement );
-      this._uiService.scrollToElement( element );
-    }
+    const canvas = select('#mlbStatsExplorer').append('svg');
+    canvas.attr('id', 'test');
+    this._httpClient.get<any>('https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json')
+      .subscribe( data => {
+      const group = canvas.selectAll('g')
+        .data(data.features)
+        .enter()
+        .append('g');
+      const projection = geoMercator();
+      const path = geoPath().projection(projection);
+
+      const areas = group.append('path')
+        .attr('d', path)
+        .attr('class', 'area')
+        .attr('fill', 'green');
+    });
   }
 
 }
