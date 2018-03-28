11ef21b9df17044f27af6bbe8bbe47796ae2651a
Marcus Wilson <mswilson4040@hotmail.com>
2018-03-27 19:00:09 -0700

    added borders to states; styling

--- src/app/views/mlbstats-explorer/mlbstats-explorer.component.ts
+++ src/app/views/mlbstats-explorer/mlbstats-explorer.component.ts
@@ -1,6 +1,6 @@
 // https://bl.ocks.org/mbostock/4408297
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ChadwickService } from '../../shared-services/chadwick.service';
 import { UIService } from '../../shared-services/ui.service';
 import { HttpClient } from '@angular/common/http';
@@ -12,12 +12,19 @@ import * as topojson from 'topojson/dist/topojson.js';
   selector: 'app-mlbstats-explorer',
   templateUrl: './mlbstats-explorer.component.html',
   styleUrls: ['./mlbstats-explorer.component.scss'],
-  providers: [ ChadwickService, GeoLocationService ]
+  providers: [ ChadwickService, GeoLocationService ],
+  encapsulation: ViewEncapsulation.None
 })
 export class MlbstatsExplorerComponent implements OnInit {
-
+  private _centered: boolean;
+  private _path: any;
+  private _height = 960;
+  private _width: 500;
+  private _svg: any;
   constructor(private _chadwickService: ChadwickService, private _uiService: UIService, private _httpClient: HttpClient,
-              private _geoLocationService: GeoLocationService) { }
+              private _geoLocationService: GeoLocationService) {
+    this._width = 500;
+  }
 
   async ngOnInit() {
 
@@ -27,33 +34,57 @@ export class MlbstatsExplorerComponent implements OnInit {
       }
     ));
     const topoJsonBallparks = topojson.topology({ballparks: geoReq});
-
+    const us: any = await this._httpClient.get('https://bl.ocks.org/mbostock/raw/4090846/us.json').toPromise();
     if (true) {
-      const height = 960;
-      const width = 500;
-      const projection = d3.geoAlbers();
-      const path = d3.geoPath().projection(projection).pointRadius(2.5);
-      const svg = d3.select('svg')
-        .attr('width', width)
-        .attr('height', height);
-      const us: any = await this._httpClient.get('https://bl.ocks.org/mbostock/raw/4090846/us.json').toPromise();
-      svg.append('path')
+
+      const projection = d3.geoAlbersUsa();
+      this._path = d3.geoPath().projection(projection).pointRadius(2.5);
+      this._svg = d3.select('svg')
+        .attr('width', this._width)
+        .attr('height', this._height);
+
+      this._svg.append('path')
         .datum(topojson.feature(us, us.objects.land))
         .attr('class', 'land')
-        .attr('d', path);
+        .attr('d', this._path)
+        .on('click', this.onStateClick.bind(this));
 
-      svg.append('path')
+      this._svg.append('path')
         .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b ))
         .attr('class', 'states')
-        .attr('d', path);
-      console.log(topoJsonBallparks);
-      svg.append('path')
+        .attr('d', this._path);
+
+      this._svg.append('path')
         .datum(topojson.feature(topoJsonBallparks, topoJsonBallparks.objects.ballparks))
         .attr('class', 'points')
-        .attr('fill', 'red')
-        .attr('d', path);
+        .attr('d', this._path);
 
     }
+  }
+  onStateClick(d) {
+    console.log(this._width);
+    let x;
+    let y;
+    let k;
+    if (d && this._centered !== d) {
+      const centroid = this._path.centroid(d);
+      x = centroid[0];
+      y = centroid[1];
+      k = 4;
+      this._centered = d;
+    } else {
+      x = this._width / 2;
+      y = this._height / 2;
+      k = 1;
+      this._centered = null;
+    }
+    this._svg.selectAll('path')
+      .classed('active', this._centered && function (z) { return z === this._centered; } );
+
+    this._svg.transition()
+      .duration(750)
+      .attr('transform', 'translate(' + this._width / 2 + ',' + this._height / 2 + ')scale(' + k + ')translate(' + -x + ',' + -y + ')')
+      .style('stroke-width', 1.5 / k + 'px');
   }
 
 }
